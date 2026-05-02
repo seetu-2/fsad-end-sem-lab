11b789fea61ef05c5efdc3ff02c6fafe2009b710
seetu-2 <psvsritha2@gmail.com>
2026-05-02 18:37:59 +0530

    Added Shipment entity

---
+++ src/main/java/com/klef/fsad/exam/Shipment.java
@@ -0,0 +1,21 @@
+package com.klef.fsad.exam;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Shipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String status;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
